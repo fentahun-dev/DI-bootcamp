ecfc1d0f765cc5c352119264b20a5202b9773b7d
fentahun <fentahun111@gmail.com>
2026-08-18 20:16:37 +0300

    first commit

---
+++ week1/Exercise XP Gold/main.py
@@ -0,0 +1,23 @@
+
+print("Hello World ")
+print("Hello World ")
+print("Hello World ")
+print("Hello World ")
+print("I Love Python")
+print("I Love Python")
+print("I Love Python")
+print("I Love Python")
+print("I Love Python")
+
+month = int (input ("Enter month(1-12):"))
+if month in [12,1,2]:
+    print ("winter")
+elif month in [ 3,4,5 ]:
+    print("spring")
+elif month in [ 6, 7 , 8]:
+    print("summer")
+else: month in [ 9,10,11]
+print("Autumn")
+
+
+
---
+++ week1/day2/ExercisesXP/main.py
@@ -0,0 +1,44 @@
+print("hello world")
+result = (99 ** 3) * 8
+print(result)
+print(15 < 8 )
+print(5 < 3 )
+print( 3 == 3)
+print( 3 == "3") 
+
+print( "Hello" == "hello")
+computer_brand = "Dell"
+print(f" I have a {computer_brand } computer ")
+name = "Fentahun"
+age = "34"
+shoe_size = "42"
+info = f"My name is {name },I am {age} years old,and I have an {computer_brand} computer."
+print(info)
+a = 8
+b = 4 
+if a > b:
+    print("hello world ")
+
+    number = int(input("Enter a number"))
+    if number % 2 == 0 :
+       print ("Even")
+    else:
+        print("Odd")   
+
+        name = input(" Your name ")
+        if name == "Dani":
+            print(" Hhh same name ")
+        else: print("Nice to meet you ! ")    
+
+
+        height = int( input ( " Enter your height in cm:"))
+        if height > "150 cm":
+           print ("You are tall !! ")
+        else:
+            print(" You need grow more !! ")
+
+        
+
+        
+    
+            
