a5a288d769f5d00d63adf62e28ea5acc2147f596
fentahun <fentahun111@gmail.com>
2026-08-28 17:20:18 +0300

    Update favorite numbers and combine with friend's
    Removed the number 6 from my_fav_numbers and created our_fav_numbers by combining my_fav_numbers and friend_fav_numbers.

--- week1/day4/Exercises/main.py
+++ week1/day4/Exercises/main.py
@@ -1,3 +1,5 @@
-my_fav_numbers={1,2,3,4,}
+my_fav_numbers = {1,2,3,4}
 my_fav_numbers.update({5,6})
-print(my_fav_numbers)
+my_fav_numbers.remove(6)
+friend_fav-numbers = {2,4,7,8 }
+our_fav_numbers = my_fav_numbers | friend_fav_numbers
