dab5fcfbe7d717cf1f9e9d07bed2faeffda6dc5d
fentahun <fentahun111@gmail.com>
2026-08-30 00:45:36 +0300

    Update main.py
    Updated favorite numbers and modified basket operations.

--- week1/day4/Exercises/main.py
+++ week1/day4/Exercises/main.py
@@ -1,5 +1,22 @@
+fav_numbers = {2,4,5,8}
+fav_numbers.add(10)
+fav_numbers.remove(2)
+
 my_fav_numbers = {1,2,3,4}
 my_fav_numbers.update({5,6})
 my_fav_numbers.remove(6)
 friend_fav-numbers = {2,4,7,8 }
 our_fav_numbers = my_fav_numbers | friend_fav_numbers
+
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("banana","Blueberries")
+basket.insert( 0,"Apples" )
+basket.append("Kiwi")
+print(basket_counts[Apples])
+basket.clear()
+print(basket)
+
+mix_numbers = [ 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+mix_numbers = range(0,len(mix_numbers))
+
+
