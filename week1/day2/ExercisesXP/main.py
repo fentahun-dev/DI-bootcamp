b44f2d1210127630df830c6775b2e02b10d171c1
fentahun <fentahun111@gmail.com>
2026-09-06 23:31:57 +0300

    checks

--- week1/day2/ExercisesXP/main.py
+++ week1/day2/ExercisesXP/main.py
@@ -1,12 +1,13 @@
 print("hello world" * 4)
+
 result = (99 ** 3) * 8
 print(result)
-print(15 < 8 )
-print(5 < 3 )
-print( 3 == 3)
-print( 3 == "3") 
+print(15 < 8 ) #True
+print(5 < 3 ) #True
+print( 3 == 3) #True
+print( 3 == "3") #False
+print( "Hello" == "hello")#False
 
-print( "Hello" == "hello")
 computer_brand = "Dell"
 print(f" I have a {computer_brand } computer ")
 name = "Fentahun"
@@ -14,10 +15,11 @@ age = "34"
 shoe_size = "42"
 info = f"My name is {name },I am {age} years old,and I have an {computer_brand} computer."
 print(info)
+
 a = 8
 b = 4 
 if a > b:
-    print("hello world ")
+    print("Hello world ")
 
     number = int(input("Enter a number"))
     if number % 2 == 0 :
@@ -32,7 +34,7 @@ if a > b:
 
 
         height = int( input ( " Enter your height in cm:"))
-        if height > "150 cm":
+        if height > 145:
            print ("You are tall !! ")
         else:
             print(" You need grow more !! ")
