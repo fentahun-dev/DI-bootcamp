8cf02c003c7985168b46f6b6c799da02be15e9ec
fentahun <fentahun111@gmail.com>
2026-09-17 23:48:58 +0300

    exercise
    Added exercises for concatenating lists and finding numbers in a range.

--- week1/day4/Exercises/Exercises XP Gold.py
+++ week1/day4/Exercises/Exercises XP Gold.py
@@ -1,1 +1,11 @@
+# exercise one concatenate lists 
+a = [ 1,2,3,4]
+b = [5,6,7,8]
+c = [*a,*b]
 
+# exercise 2 range of numbers 
+for i in range(1500,2501):
+  if i % 5 == 0 and i % 7 == 0:
+    print(i)
+
+# exercise check the index 
